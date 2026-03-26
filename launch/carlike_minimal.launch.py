2935ac0009fb73b51e2a4c02f8fd23ec22cc0d5c
Daniel Mascarenhas <daniel.mascarenhas@saipem.com>
2026-03-26 17:35:50 -0300

    adding a first simple package at ros2
    Signed-off-by: Daniel Mascarenhas <daniel.mascarenhas@saipem.com>

---
+++ launch/carlike_minimal.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    xacro_file = PathJoinSubstitution([
+        FindPackageShare('lar_car_like2'),
+        'urdf',
+        'carlike_minimal.urdf.xacro'
+    ])
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': Command(['xacro ', xacro_file])
+        }]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', PathJoinSubstitution([
+            FindPackageShare('lar_car_like2'),
+            'rviz',
+            'robot_minimal.rviz'
+        ])]
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        rviz_node
+    ])
